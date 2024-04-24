e356f57b6cb2bcac1f5e111286e07209e2042bc9
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-04-24 09:22:07 +0200

    FEAT: add "Hello World" function in Main class

---
+++ Main.java
@@ -0,0 +1,8 @@
+// WITH CODE RUNNER (VSCODE PLUGGIN)
+// CTRL + ALT + N >>> compilet and run
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
